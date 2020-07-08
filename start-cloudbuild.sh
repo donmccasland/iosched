4e37a78caaff97942ee09852adc20166ada6426a
Don McCasland <donmccasland@google.com>
2020-07-08 16:14:13 -0700

    Adjusting build parameters

--- start-cloudbuild.sh
+++ start-cloudbuild.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcloud builds submit --config=cloudbuild.yaml --substitutions=_ARTIFACT_BUCKET=282019-build-artifacts,_CACHE_BUCKET=282019-build-cache 2>&1 | tee build.log
+gcloud builds submit --config=cloudbuild.yaml --substitutions=_ARTIFACT_BUCKET=282019-build-artifacts,_CACHE_BUCKET=282019-build-cache,_BRANCH_NAME=main 2>&1 | tee build.log
 
